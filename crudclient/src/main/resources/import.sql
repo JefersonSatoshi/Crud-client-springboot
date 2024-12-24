e50eecdf17221c052df70082afb6f7b9ff7aad63
“jeferson” <jefersonsatoshi@hotmail.com>
2024-12-24 17:48:44 -0300

    ambiente de testes, H2 e import.sql

---
+++ crudclient/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria Silva', '11223344556', '2000.0', '1995-05-12', '1');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('João Pereira', '12345678901', '3500.0', '1988-03-14', '2');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Ana Oliveira', '98765432100', '2800.0', '1992-07-19', '0');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Carlos Souza', '45678912302', '4000.0', '1985-11-23', '3');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Beatriz Lima', '23456789012', '3200.0', '1990-05-30', '1');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Fernando Alves', '11234567890', '5000.0', '1978-04-22', '2');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Gabriela Ferreira', '99887766554', '2700.0', '1994-09-10', '0');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Ricardo Santos', '77665544332', '4500.0', '1981-12-01', '4');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Patrícia Moreira', '55443322110', '3100.0', '1989-06-25', '2');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('José Mendes', '33445566778', '3800.0', '1993-08-14', '1');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Mariana Costa', '22334455667', '2900.0', '1996-01-05', '0');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Paulo Rocha', '77889900112', '4200.0', '1983-03-18', '3');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Clara Gomes', '88990011223', '2500.0', '1991-10-08', '1');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Rodrigo Ribeiro', '66778899001', '3300.0', '1986-07-20', '2');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Amanda Silva', '55667788990', '3700.0', '1995-02-15', '0');
